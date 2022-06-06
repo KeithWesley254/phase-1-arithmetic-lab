a25352782592b052ebd6b1ade5939473e8bc5454
Keith <keithwesley187@gmail.com>
2022-06-06 21:10:48 +0300

    commit tree f60ef098e7403fa922c091b47b46e391e2d00756

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+//multiply
+
+let num1= 31
+let num2= 2
+const multiply= num1 * num2
+
+//random
+
+var random;
+
+random= Math.floor(Math.random() * 10) + 1
+
+console.log(random)
+
+//mod
+
+let num3= 22
+let num4= 18
+
+const mod= num3 % num4
+
+//max
+
+var max;
+
+max= Math.max(1,5,7,9,17,20);
+
+console.log(max)
